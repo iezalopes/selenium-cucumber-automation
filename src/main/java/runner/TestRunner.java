d4161d86191b049e01e335c8649f110f4d2ef931
iezalopes <ieza.lopes.7@gmail.com>
2020-08-17 10:19:28 -0300

    resolve pom file

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -3,6 +3,7 @@ package runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
+
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
